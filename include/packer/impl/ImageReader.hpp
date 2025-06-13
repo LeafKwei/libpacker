f70366934ddc499ecd39a1e5a61496db7b1baa8d
Leaf Kwei <leaf.kwei@outlook.com>
2025-06-13 15:55:52 +0800

    Change some descriptions.

--- include/packer/impl/ImageReader.hpp
+++ include/packer/impl/ImageReader.hpp
@@ -5,11 +5,11 @@
 PACKER_BEGIN
 
 /**
- * ImageReader实现了read函数，将read函数拆分为了三个接口留给后续派生类实现，其中，initialize要求
+ * ImageReader实现了read函数，将read函数拆分为了三个接口留给后续派生类实现，其中，beforeRead要求
  * 派生类执行像素数据拷贝前的初始化，例如打开具体的图片文件，然后将图片的宽高通过width和height两个
  * 引用传递给ImageReader，随后ImageReader会根据宽高创建一个Image对象，并将Image对象的RGBA内
- * 存块通过readAllRGB函数传递给派生类，由派生类负责将图片的像素数据填充到RGBA内存块中，最后，
- * ImageReader调用finalize函数执行派生类定义的清理工作
+ * 存块通过readRGB函数传递给派生类，由派生类负责将图片的像素数据填充到RGBA内存块中，最后，
+ * ImageReader调用afterRead函数执行派生类定义的清理工作
  */
 class ImageReader : public VImageReader{
 public:
